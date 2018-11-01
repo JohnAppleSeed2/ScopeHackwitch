dc96ed820d63f00a03b86b6dd63905139be56cc0
JOHN SANCHEZ <rl200295@rlas-macair-29.local>
2018-11-01 10:28:33 -0500

    Initial Commit

---
+++ ScopeHackwitch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScopeHackwitch
+//
+//  Created by JOHN SANCHEZ on 11/1/18.
+//  Copyright © 2018 JOHN SANCHEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
